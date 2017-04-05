3f9cc58c5d601e5331263fc5da2bff9ba63c0836
Daniel Carlson <dancarl758@gmail.com>
2017-04-05 09:44:12 +0100

    Made the UIScrollView better

--- UIScrollView/UIScrollView/ViewController.swift
+++ UIScrollView/UIScrollView/ViewController.swift
@@ -15,8 +15,11 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
         var contentWidth: CGFloat = 0.0
+        let scrollWidth = scrollView.frame.size.width
         
         for x in 0...2 {
             let image = UIImage(named: "icon\(x).png")
@@ -25,17 +28,17 @@ class ViewController: UIViewController {
             
             var newX: CGFloat =  0.0
             
-            newX = view.frame.midX + view.frame.size.width * CGFloat(x)
+            newX = scrollWidth / 2 + scrollWidth * CGFloat(x)
             
             contentWidth += newX
             
             scrollView.addSubview(imageView)
             
-            imageView.frame = CGRect(x: newX - 75, y: (view.frame.size.height / 2) - 75, width: 150, height: 150)
+            imageView.frame = CGRect(x: newX - 75, y: (scrollView.frame.size.height / 2) - 75, width: 150, height: 150)
         }
         
+        scrollView.clipsToBounds = false
         scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
-        
     }
 
     override func didReceiveMemoryWarning() {
