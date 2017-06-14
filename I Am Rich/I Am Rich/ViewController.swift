c70a07ca07c0a9e2001d1facc4ec8dcd02159722
Daniel Carlson <dancarl758@gmail.com>
2017-06-14 17:20:46 +0100

    Added simple games:Dicee and Magic 8 Ball

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Daniel Carlson on 6/13/17.
+//  Copyright © 2017 Daniel Carlson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
